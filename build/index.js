b3143090786154048e069692036fa5a5b2abf34d
Meathill <meathill@gmail.com>
2022-12-01 10:05:49 +0800

    bug: fixed redirects

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const {readdir} = require('fs/promises');
+const {readdir, writeFile} = require('fs/promises');
 const {resolve} = require('path');
 const {promisify} = require('util');
 const exec = promisify(require('child_process').exec);
@@ -10,6 +10,7 @@ const exec = promisify(require('child_process').exec);
   const files = await readdir(root, {
     withFileTypes: true,
   });
+  const redirect = [];
   for (const file of files) {
     // use `_xxx` as draft, should not be published
     if (!file.isFile() || !file.name.endsWith('.md') || file.name.startsWith('_') || file.name.startsWith('README')) {
@@ -20,6 +21,7 @@ const exec = promisify(require('child_process').exec);
     console.log('publishing', file.name);
     const to = file.name.replace(/\.md$/, '');
     const cmd = `slidev build ${file.name} -o dist/${to} --base /my-workshop/${to}/`;
+    redirect.push(to);
     console.log('cmd:', cmd);
     const {stdout, stderr} = await exec(cmd);
     if (stderr) {
@@ -28,4 +30,8 @@ const exec = promisify(require('child_process').exec);
     console.log(stdout);
     console.log('published', file.name);
   }
+
+  // write redirect file
+  const redirectFile = redirect.map((to) => `/my-workshop/${to}/*    /my-workshop/${to}/index.html    200`).join('\n');
+  await writeFile(resolve(root, 'dist/_redirects'), redirectFile, 'utf8');
 })();
